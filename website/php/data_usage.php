ca6bdb70b34cac6ae227034f508dec35f55116e0
Peter Kydas <s3485580@student.rmit.edu.au>
2017-09-04 15:15:51 +1000

    cleaning up file structure slightly

--- website/php/data_usage.php
+++
@@ -1,3 +0,0 @@
-<?php  
-	echo "Some crap";
-?>
--- website/php/header.php
+++
@@ -1,8 +0,0 @@
-<!-- Required meta tags -->
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-<!-- Get the bootstrap style sheet -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-
-<link rel='stylesheet' href='../stylesheet.css'>
